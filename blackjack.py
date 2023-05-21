f5534635c251f8fe298abb4feced43a617195e43
Alexandre Canacaris <tiloucanac1300@gmail.com>
2023-05-21 02:55:26 +0200

    good one

--- blackjack.py
+++ blackjack.py
@@ -50,35 +50,64 @@ class Hand:
 class Bot:
     def __init__(self, name, balance=100):
         self.name = name
-        self.hand = Hand()
+        self.hands = [Hand()]
         self.balance = balance
-        self.bet = 0
+        self.bets = [0]
 
-    def make_bet(self):
+    def make_bet(self, hand_index=0):
         if self.balance >= 10:
-            self.bet = random.randint(10, min(50, self.balance))  # Bets a random amount between 10 and 50 (or remaining balance if it's less than 50)
+            self.bets[hand_index] = random.randint(10, min(50, self.balance))  # Bets a random amount between 10 and 50 (or remaining balance if it's less than 50)
         else:
-            self.bet = self.balance  # If balance is less than 10, bet the remaining balance
-        self.balance -= self.bet
-        print(f"{self.name} bets {self.bet}. Remaining balance: {self.balance}")
+            self.bets[hand_index] = self.balance  # If balance is less than 10, bet the remaining balance
+        self.balance -= self.bets[hand_index]
+        print(f"{self.name} bets {self.bets[hand_index]} on hand {hand_index+1}. Remaining balance: {self.balance}")
 
-    def make_decision(self):
-        return 'h' if self.hand.value < 17 else 's'
-
-    def add_card(self, card):
-        self.hand.add_card(card)
+    def add_card(self, card, hand_index=0):
+        self.hands[hand_index].add_card(card)
         print(f"{self.name} hits a card.")
-        if self.hand.value > 21:
-            self.hand.adjust_for_ace()
+        if self.hands[hand_index].value > 21:
+            self.hands[hand_index].adjust_for_ace()
 
-    def show_hand(self, hide_first_card=False):
-        print(f"\n{self.name}'s Hand:")
-        if hide_first_card:
+    def can_split(self, hand_index=0):
+        return len(self.hands[hand_index].cards) == 2 and self.hands[hand_index].cards[0].rank == self.hands[hand_index].cards[1].rank and self.balance >= self.bets[hand_index]
+
+    def split_hand(self, hand_index=0):
+        self.hands.append(Hand())
+        self.bets.append(self.bets[hand_index])
+        self.balance -= self.bets[hand_index]
+        self.hands[-1].add_card(self.hands[hand_index].cards.pop())
+        print(f"{self.name} splits hand {hand_index+1}. New bet of {self.bets[-1]} placed on new hand.")
+
+    def can_double(self, hand_index=0):
+        return len(self.hands[hand_index].cards) == 2 and self.balance >= self.bets[hand_index]
+
+    def double_bet(self, hand_index=0):
+        self.balance -= self.bets[hand_index]
+        self.bets[hand_index] *= 2
+        print(f"{self.name} doubles bet on hand {hand_index+1} to {self.bets[hand_index]}. Remaining balance: {self.balance}")
+
+    def make_decision(self, hand_index):
+        value = self.hands[hand_index].value
+        # Check for split
+        if len(self.hands[hand_index].cards) == 2 and self.hands[hand_index].cards[0].rank == self.hands[hand_index].cards[1].rank and self.balance >= self.bets[hand_index]:
+           # Bot will split if it can and if the cards have a value of 8 or more, or the value is 4 (meaning two twos or two threes)
+           if values[self.hands[hand_index].cards[0].rank] >= 8 or values[self.hands[hand_index].cards[0].rank] == 4:
+              return 'p'
+        #Check for double
+        if value >= 9 and value <= 11 and self.balance >= self.bets[hand_index]:
+           return 'd'
+         # Check for hit or stand
+        return 'h' if value < 17 else 's'
+
+
+    def show_hand(self, hand_index=0, hide_first_card=False):
+        print(f"\n{self.name}'s Hand {hand_index+1}:")
+        if hide_first_card and hand_index == 0:
             print(' <card hidden>')
-            print('', self.hand.cards[1])
+            print('', self.hands[hand_index].cards[1])
         else:
-            print(self.hand)
-        print(f"{self.name}'s Hand Value: {self.hand.value}")
+            print(self.hands[hand_index])
+        print(f"{self.name}'s Hand Value: {self.hands[hand_index].value}")
 
     def __str__(self):
         return self.name
@@ -108,51 +137,67 @@ class Game:
                 bot.show_hand()
 
             for bot in bots:
-                print(f"\n{bot.name}'s turn:")
-                while bot.make_decision() == 'h':
-                    bot.add_card(self.deck.deal())
-                    bot.show_hand()
-                    if bot.hand.value > 21:
-                        print("Bust!")
-                        break
-                else:
-                    print("Stand!")
+                for i in range(len(bot.hands)):
+                    print(f"\n{bot.name}'s turn for hand {i+1}:")
+                    while True:
+                        decision = bot.make_decision(i)
+                        if decision == 'p':
+                            bot.split_hand(i)
+                            bot.add_card(self.deck.deal(), i)
+                            bot.add_card(self.deck.deal(), -1)
+                            bot.show_hand(i)
+                            bot.show_hand(-1)
+                        elif decision == 'd':
+                            bot.double_bet(i)
+                            bot.add_card(self.deck.deal(), i)
+                            bot.show_hand(i)
+                            break
+                        elif decision == 'h':
+                            bot.add_card(self.deck.deal(), i)
+                            bot.show_hand(i)
+                            if bot.hands[i].value > 21:
+                                print("Bust!")
+                                break
+                        else:
+                            print("Stand!")
+                            break
 
             print("\nDealer's turn:")
-            while dealer.hand.value < 17:
+            while dealer.hands[0].value < 17:
                 dealer.add_card(self.deck.deal())
-                dealer.hand.adjust_for_ace()
+                dealer.hands[0].adjust_for_ace()
             dealer.show_hand()
 
-            dealer_score = dealer.hand.value
+            dealer_score = dealer.hands[0].value
             print("\nDealer's Final Hand:")
-            print(dealer.hand)
+            print(dealer.hands[0])
 
             print("\n--- Results ---")
 
-            if dealer_score > 21:
-                print("Dealer busts! All bots win!")
-                for bot in bots:
-                    if bot.hand.value <= 21:
-                        bot.balance += bot.bet * 2
-            else:
-                for bot in bots:
-                    bot_score = bot.hand.value
-                    if bot_score > 21:
+            for bot in bots:
+                for i, hand in enumerate(bot.hands):
+                    bot_score = hand.value
+                    print(f"\nResults for {bot.name}'s hand {i+1}:")
+                    if dealer_score > 21:
+                        print("Dealer busts! Bot wins!")
+                        bot.balance += bot.bets[i] * 2
+                    elif bot_score > 21:
                         print(f"{bot.name} busts!")
                     elif bot_score > dealer_score:
                         print(f"{bot.name} wins!")
-                        bot.balance += bot.bet * 2
+                        bot.balance += bot.bets[i] * 2
                     elif bot_score == dealer_score:
-                        print(f"{bot.name} ties with the dealer!")
-                        bot.balance += bot.bet  # Return the bet to the bot
+                        print("Push! It's a tie.")
+                        bot.balance += bot.bets[i]
                     else:
-                        print(f"{bot.name} loses!")
+                        print(f"{bot.name} loses.")
+                bot.hands = [Hand()]
+                bot.bets = [0]
 
             print("\n--- End of round ---")
-
+            print("\n--- Balances ---")
             for bot in bots:
-                print(f"{bot.name}'s remaining balance: {bot.balance}")
+                print(f"{bot.name}'s balance: {bot.balance}")
 
             self.deck = Deck()  # Refresh the deck for the next round
 
